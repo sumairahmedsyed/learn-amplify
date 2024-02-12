e488efcd9770119bb06099e01c913d3b7c669bf1
Sumair Syed <ssy@SVMBP15A.sv.domain>
2024-02-12 16:16:39 +1100

    Added auth

--- my-angular-amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ my-angular-amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myangularamplifyapp2f291fb7": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
