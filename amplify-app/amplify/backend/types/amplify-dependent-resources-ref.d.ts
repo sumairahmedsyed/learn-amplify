4c8eb1c6dde27fb2919363b7004dd19686a7f69c
Sumair Syed <ssy@SVMBP15A.local>
2024-02-16 13:00:36 +1100

    Added amplify auth changes

---
+++ amplify-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyapp6a3d9742": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
